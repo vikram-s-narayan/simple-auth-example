c8aa58db860414970d1d69778da73efcc262996c
vikram-s-narayan <vikram@freado.com>
2015-11-27 15:32:05 +0530

    first commit

---
+++ app/routes/application.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+       login: function() {
+         var controller = this.get('controller');
+         var email = controller.get('userEmail');
+         var password = controller.get('userPassword');
+           this.get('session').open('firebase', {
+                provider: 'password',
+                email: email,
+                password: password
+           }).then(function() {
+               this.transitionTo('protected');
+           }.bind(this));
+       },
+       logout: function() {
+           this.get('session').close().then(function() {
+               this.transitionTo('application');
+           }.bind(this));
+       }
+   }
+});
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,11 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'my-auth-test2',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://YOUR-FIREBASE.firebaseio.com/',
+    torii: {
+      sessionServiceName: 'session'
+    },
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
